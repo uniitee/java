3a349245e7ae9dc6e9df916d3f0a8db8f2f68a70
UmaGhildiyal <ghildiyaluma0@gmail.com>
2025-02-07 13:03:43 +0530

    Packages and exception

---
+++ tutorial/CLA.java
@@ -0,0 +1,8 @@
+public class CLA{
+    public static void main(String[] args){
+        System.out.println("Number of arguments: " + args.length);
+        for(int i = 0; i < args.length; i++){
+            System.out.println("Argument " + i + ": " + args[i]);
+        }
+    }
+}
