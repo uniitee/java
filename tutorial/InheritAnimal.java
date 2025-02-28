b2f21cd451a396421024c1659ec5c47c75c46ec0
UmaGhildiyal <ghildiyaluma0@gmail.com>
2025-02-28 11:57:20 +0530

    dynamic method dispatch

--- tutorial/InheritAnimal.java
+++ tutorial/InheritAnimal.java
@@ -1,19 +1,45 @@
 class Animal {
     public void sound() {
         System.out.println("Animal is making a sound");
-    }    
+    } 
+    public void type(){
+        System.out.println("Mamal");
+    }
 }
 
 class Dog extends Animal {
     public void sound() {
+        super.sound(); // only way to acess the super class sound
         System.out.println("Dog is barking");
+    }
+    public void isCute(){
+        System.out.println("Very Cute");
     }
 }
 
 public class InheritAnimal {
     public static void main(String[] args) {
+
+        // ___________________ALLOWED_______________________
+        // Animal ani = new Animal(); //created at runtime
+        // ani.sound();
+        // ani.type();
+
+        // Dog doggo = new Dog(); //created at runtime
+        // doggo.sound();
+        // doggo.isCute();
+
+        // Animal ani = new Dog(); // reference -> super-class | obj -> subclass
+        // _________________________________________________
+        
+        // __________________NOT ALLOWED____________________
+        // Dog doge = new Animal(); // NOT ALLOWED
+        // _________________________________________________
+
         Animal d = new Dog();
-        d.sound();
+        d.sound();      // obj ka run hoga method not of reference
+        // d.isCute();  // error => reference was of Animal so method dog cannot be called
+        d.type();       // called of super class
         // d.super.sound(); // an error
     }
 }
