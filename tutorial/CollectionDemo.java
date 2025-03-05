3b49a3372928b8d74da45fa33810189895898205
UmaGhildiyal <ghildiyaluma0@gmail.com>
2025-03-05 11:12:05 +0530

    Add CollectionDemo class to demonstrate ArrayList usage with Animal objects

---
+++ tutorial/CollectionDemo.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Animal{
+    String name;
+    Animal(String n){
+        this.name = n;
+    }
+}
+public class CollectionDemo {
+    public static void main(String[] agrs){
+        ArrayList a1 = new ArrayList();
+        // a1.add(Animal("dog"));
+        a1.add(new Animal("DOG"));
+        a1.add(new Animal("BAT"));
+        a1.add(new Animal("RAT"));
+        a1.add(new Animal("CAT"));
+        a1.add("b");
+        a1.add("");
+        a1.add("c");
+        a1.add("d");
+        
+        // Iterator<String> itr = a1.iterator();
+        // while (itr.hasNext()){
+        //     System.out.println(itr.next());
+        // }
+
+        System.out.println(a1);
+    }
+    // public static void main(String[] agrs){
+    //     ArrayList a1 = new ArrayList();
+    //     // a1.add(Animal("dog"));
+    //     a1.add("a");
+    //     a1.add("b");
+    //     a1.add("");
+    //     a1.add("c");
+    //     a1.add("d");
+    //     Iterator<String> itr = a1.iterator();
+    //     while (itr.hasNext()){
+    //         System.out.println(itr.next());
+    //     }
+    // }
+}
